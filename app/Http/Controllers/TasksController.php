d69c41a00f3f33015eb573eec70d1b142864376e
fusen daniel <fusendaniel20020115@gmail.com>
2020-09-27 04:25:10 +0000

    sample

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -12,4 +12,27 @@ class TasksController extends Controller
       $tasks = Task::all();
       return view('tasks.index')->with('tasks',$tasks);
   }
+  
+  //todo追加する処理
+  public function store(Request $request){
+    $task = new Task();
+    $task->body = $request->body;
+    $task->save();
+    return redirect('/');
+  }
+  //削除処理
+  public function destroy (task $task) {
+    $task->delete();
+    return redirect('/');
+  }
+  //編集画面の変移処理
+  public function edit(task $task){
+    return view('tasks.edit')->with('task',$task);
+  }
+  //更新処理
+  public function update (Request $request,task $task){
+    $task->body =$request->body;
+    $task->save();
+    return redirect('/');
+  }
 }
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -18,7 +18,7 @@
           <label> やることを追加してください </label>
           <input type="text" name="body" class ="form-control" placeholder="todo list" style="max-with:1000px;"/>
         </div>
-        <button type="text" name="body" class="form-control" placeholder="todo list" style="max-with:1000px"></button>
+        
        </form>
       <h1 style="margin-top:50px;">Todoリスト</h1>
       <table class="table table-striped" style="max-width:1000px; margin-top:20px;">
@@ -42,7 +42,7 @@
         <td><form action="{{url('/tasks',$task->id)}}"method="post">
            {{ csrf_field() }}
            {{ method_field('delete') }}
-           <button type="submit" class="btn btn-danger">完了</button>
+           <button type="submit" class="btn text-danger">完了</button>
          </form>
          </td>
          <!-- 削除した際にポップ画面で確認をする -->
@@ -65,5 +65,27 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script>
+      'use strict';
+      
+      var cmds = document.getElementsByClassName('del');
+      var i;
+      
+      for(i = 0 ; i < cmds.length; i++){
+        
+        cmds[i].addEventListener('click',function(e){
+          
+          e.preventDefault();
+          if (confirm('よろしいですか？')){
+            document.getElementById('form_'+this.dataset.id).submit();
+          }
+          
+        }
+        );
+        
+      }
+      
+      
+    </script>
   </body>
 </html>
